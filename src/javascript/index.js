011e106df48ed73f41dfb99e1ad6a8e43b880c47
Kristoffer Søholm Gillesberg <kristoffer6400@gmail.com>
2025-03-07 09:51:13 +0100

    Basic index navigation function. Index.html has 0 API content

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,1 +1,29 @@
 console.log("er i index.js");
+
+document.addEventListener("DOMContentLoaded", () => {
+    const indexLinks = document.querySelectorAll(".nav-links a");
+
+    indexLinks.forEach(link => {
+        link.addEventListener("click", (event) => {
+            event.preventDefault();
+
+            const linkText = link.textContent.trim();
+            if (linkText === "Forside") {
+                navigateToIndex();
+            } else if (linkText === "Program") {
+                navigateToProgram();
+            }
+        });
+    });
+});
+
+// Function for navigating to index.html
+function navigateToIndex() {
+    window.location.href = "index.html";
+}
+
+
+//to have API call to add data for the calender
+function navigateToProgram() {
+
+}
